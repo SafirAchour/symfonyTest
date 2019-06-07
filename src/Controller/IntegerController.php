ad0991b098964de80f9f23ff57535814c1411f63
Saphir Achour <achoursaphir@gmail.com>
2019-06-07 16:20:03 +0200

    Review IntegerCheck after hydrating tables

--- src/Controller/IntegerController.php
+++ src/Controller/IntegerController.php
@@ -7,7 +7,20 @@ use Symfony\Component\HttpFoundation\Response;
 
 class IntegerController
 {
-    public function integerCheck($id)
+    /* Page Loading with working with lucky number generator Function
+     * 
+     * 
+     * TO DEBUG: 
+     * 
+     * With $id in function argument: 
+     * Could not resolve argument $id of "App\Controller\IntegerController::integercheck()",
+     *  maybe you forgot to register the controller as a service or missed tagging it with the "controller.service_arguments"?
+     *  
+     *  
+     *  With no function argument:
+     *  The controller must return a "Symfony\Component\HttpFoundation\Response" object but it returned a number (0).
+     */
+    public function integerCheck()
     {
         for($id = 0; $id < 100; ++$id) {
             if (is_int($id))
--- src/Entity/Integer.php
+++ src/Entity/Integer.php
@@ -9,5 +9,21 @@ class Integer
      * 
      */
     public $id;
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
 }
 
