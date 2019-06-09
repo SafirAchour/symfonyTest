3ee2474d1f8926e086cd2cf5586481c97c622e05
Saphir Achour <achoursaphir@gmail.com>
2019-06-09 14:08:36 +0200

    Choose method Views working - To Fix: List Controller + View

--- src/Controller/BuzzController.php
+++ src/Controller/BuzzController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Buzz;
+use App\Entity\Integer;
 
 class BuzzController extends AbstractController
 {
@@ -13,7 +16,31 @@ class BuzzController extends AbstractController
     public function index()
     {
         return $this->render('buzz/index.html.twig', [
-            'controller_name' => 'BuzzController',
+            'collection' => $this->hydrate()
         ]);
+    }
+    /**
+     * @return ArrayCollection
+     */
+    public function hydrate()
+    {
+        $collection = new ArrayCollection();
+        for ($i = 1; $i <= 100; $i++) {
+            $collection->add($this->getEntity($i));
+        }
+        return $collection;
+    }
+    
+    public function getEntity(int $i)
+    {
+        $entity = new Integer();
+        
+        if (($i % 3) === 0) {
+            $entity = new Buzz();
+        }
+        
+        $entity->setNumber($i);
+        
+        return $entity;
     }
 }
--- src/Controller/FizzController.php
+++ src/Controller/FizzController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Integer;
+use App\Entity\Fizz;
 
 class FizzController extends AbstractController
 {
@@ -13,7 +16,31 @@ class FizzController extends AbstractController
     public function index()
     {
         return $this->render('fizz/index.html.twig', [
-            'controller_name' => 'FizzController',
+            'collection' => $this->hydrate()
         ]);
+    }
+    /**
+     * @return ArrayCollection
+     */
+    public function hydrate()
+    {
+        $collection = new ArrayCollection();
+        for ($i = 1; $i <= 100; $i++) {
+            $collection->add($this->getEntity($i));
+        }
+        return $collection;
+    }
+    
+    public function getEntity(int $i)
+    {
+        $entity = new Integer();
+        
+        if (($i % 3) === 0) {
+            $entity = new Fizz();
+        }
+        
+        $entity->setNumber($i);
+        
+        return $entity;
     }
 }
